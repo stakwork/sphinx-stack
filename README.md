6c9b84f56e6d8aa70cd9130bd922a5127b753a17
Evanfeenstra <evanfeenstra@gmail.com>
2021-07-16 13:48:04 -0700

    docker compose, init.sql, notes, configs

---
+++ README.md
@@ -0,0 +1,6 @@
+# start
+docker-compose up -d
+
+# to clear volumes (restart from scratch)
+docker-compose down --volumes
+
---
+++ init.sql
@@ -0,0 +1,114 @@
+-- DROP TABLE media;
+-- DROP TABLE tribes;
+-- DROP TABLE bots;
+-- DROP TABLE people;
+
+CREATE TABLE media (
+  id TEXT NOT NULL PRIMARY KEY,
+  owner_pub_key TEXT NOT NULL,
+  name TEXT,
+  description TEXT,
+  price BIGINT,
+  tags TEXT[] not null default '{}',
+  ttl BIGINT,
+  filename TEXT,
+  size BIGINT,
+  mime TEXT,
+  nonce TEXT,
+  created timestamptz,
+  updated timestamptz,
+  expiry timestamptz, -- optional permanent deletion of file
+  total_sats BIGINT,
+  total_buys BIGINT,
+  template boolean,
+  width INT,
+  height INT
+);
+
+ALTER TABLE media ADD COLUMN tsv tsvector;
+
+CREATE INDEX media_tsv ON media USING GIN(tsv);
+
+
+
+CREATE TABLE tribes (
+  uuid TEXT NOT NULL PRIMARY KEY,
+  owner_pub_key TEXT NOT NULL,
+  owner_alias TEXT,
+  group_key TEXT,
+  name TEXT,
+  description TEXT,
+  tags TEXT[] not null default '{}',
+  img TEXT,
+  price_to_join BIGINT,
+  price_per_message BIGINT,
+  escrow_amount BIGINT,
+  escrow_millis BIGINT,
+  created timestamptz,
+  updated timestamptz,
+  member_count BIGINT,
+  unlisted boolean,
+  private boolean,
+  deleted boolean,
+  app_url TEXT,
+  feed_url TEXT,
+  last_active BIGINT,
+  bots TEXT,
+  owner_route_hint TEXT,
+  unique_name TEXT
+);
+
+ALTER TABLE tribes ADD COLUMN tsv tsvector;
+
+CREATE INDEX tribes_tsv ON tribes USING GIN(tsv);
+
+
+
+CREATE TABLE bots (
+  uuid TEXT NOT NULL PRIMARY KEY,
+  owner_pub_key TEXT NOT NULL,
+  owner_alias TEXT,
+  name TEXT,
+  unique_name TEXT,
+  description TEXT,
+  tags TEXT[] not null default '{}',
+  img TEXT,
+  price_per_use BIGINT,
+  created timestamptz,
+  updated timestamptz,
+  member_count BIGINT,
+  unlisted boolean,
+  deleted boolean,
+  owner_route_hint text
+);
+
+ALTER TABLE bots ADD COLUMN tsv tsvector;
+
+CREATE INDEX bots_tsv ON bots USING GIN(tsv);
+
+
+
+
+
+CREATE TABLE people (
+  id SERIAL PRIMARY KEY,
+  owner_pub_key TEXT NOT NULL,
+  owner_alias TEXT,
+  owner_route_hint TEXT,
+  owner_contact_key TEXT,
+  description TEXT,
+  tags TEXT[] not null default '{}',
+  img TEXT,
+  created timestamptz,
+  updated timestamptz,
+  unlisted boolean,
+  deleted boolean,
+  unique_name TEXT,
+  price_to_meet BIGINT,
+  extras JSONB,
+  twitter_confirmed BOOLEAN
+);
+
+ALTER TABLE people ADD COLUMN tsv tsvector;
+
+CREATE INDEX people_tsv ON people USING GIN(tsv);
