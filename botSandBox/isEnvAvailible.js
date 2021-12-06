f68c33260e25b5bd706320a815a76a23df621e3f
Kevin Pallikunnel <oapallikunnel@gmail.com>
2021-12-06 13:00:30 -0800

    adding changes to have the bots install while in a different yml file, we also now check if the file for the bots env exists so we can add it to the gitignore now

--- botSandBox/isEnvAvailible.js
+++ botSandBox/isEnvAvailible.js
@@ -1,7 +1,14 @@
-var fs = require('fs');
+var fs = require("fs");
 
 let index = process.env.SPHINX_INDEX;
-var envVars = require('/example_bot/botEnvVars.json');
+var envVars = require("/example_bot/botEnvVars.json");
+
+if (!fs.existsSync("/example_bot/botEnvVars.json")) {
+  let envVars = require("/example_bot/botEnvVars");
+  if (envVars.length == 0) {
+    process.abort();
+  }
+}
 
 //So this script is run when the bots are initialized so we can
 //insert the SPHINX_TOKEN and PORT for the bots before they spin up
@@ -9,7 +16,7 @@ var envVars = require('/example_bot/botEnvVars.json');
 //the bot depends on this file getting to process.exit()
 if (envVars[index].SPHINX_TOKEN != null) {
   fs.writeFileSync(
-    '.env',
+    ".env",
     `SPHINX_TOKEN=${envVars[index].SPHINX_TOKEN} 
      PORT=${envVars[index].PORT}`
   );
