3e8b533fadaddb7f3514c32f56968e5c76a790e7
Kevin Pallikunnel <oapallikunnel@gmail.com>
2021-12-22 08:37:30 -0600

    checking for the authtoken and not just the file

--- relay/setup/waitForNODES.js
+++ relay/setup/waitForNODES.js
@@ -1,7 +1,12 @@
 var fs = require("fs");
+var paths = require("./paths");
 
 while (!fs.existsSync("./relay/NODES.json")) {
   console.log("Still in here");
 }
+
+do {
+  var nodes = require(paths.path);
+} while (nodes[0].authToken == null);
 
 process.exit();
