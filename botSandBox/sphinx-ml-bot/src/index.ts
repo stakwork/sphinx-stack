78c11c90eb077bea5f2ee34281ce537e28c461c6
Oluwatobi Bamidele <teebams49@gmail.com>
2023-08-14 20:15:47 +0100

    update: changed response format

--- botSandBox/sphinx-ml-bot/src/index.ts
+++ botSandBox/sphinx-ml-bot/src/index.ts
@@ -3,17 +3,19 @@ import axios from "axios";
 
 const app = express();
 
-function sendWebhook(url: string, project_id: string, msg: string) {
+app.use(express.json());
+
+function sendWebhook(url: string, process_id: string, msg: string) {
   try {
-    axios.post(url, { project_id, body: { response: msg } });
+    axios.post(url, { body: { process_id, response: msg } });
   } catch (error) {
     console.log(`Error sending webhook: ${error}`);
   }
 }
 
-function generateProjectId() {
-  const project_id = Math.random().toString(36).substring(2, 10);
-  return project_id;
+function generateProcessId() {
+  const process_id = Math.random().toString(36).substring(2, 10);
+  return process_id;
 }
 
 app.post("/test", (req: Request, res: Response) => {
@@ -21,27 +23,27 @@ app.post("/test", (req: Request, res: Response) => {
 });
 
 app.post("/text", (req: Request, res: Response) => {
-  const project_id = generateProjectId();
+  const process_id = generateProcessId();
   setTimeout(() => {
     sendWebhook(
       req.body.webhook,
-      project_id,
+      process_id,
       "This is a response from test ml-bot server built in sphinx-stack"
     );
   }, 30000);
-  return res.status(200).json({ project_id });
+  return res.status(200).json({ body: { process_id } });
 });
 
 app.post("/image", (req: Request, res: Response) => {
-  const project_id = generateProjectId();
+  const process_id = generateProcessId();
   setTimeout(() => {
     sendWebhook(
       req.body.webhook,
-      project_id,
+      process_id,
       "https://res.cloudinary.com/teebams/image/upload/v1648478325/elite/wiot5aymifdzqwplyu1a.png"
     );
   }, 30000);
-  return res.status(200).json({ project_id });
+  return res.status(200).json({ body: { process_id } });
 });
 
 const port = 3500;
