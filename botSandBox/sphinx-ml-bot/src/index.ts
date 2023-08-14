76b05de30308f6aaa087fce61dee9f2fba118bf1
Oluwatobi Bamidele <teebams49@gmail.com>
2023-08-14 16:46:17 +0100

    feat: setup express server

--- botSandBox/sphinx-ml-bot/src/index.ts
+++ botSandBox/sphinx-ml-bot/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Look who we got here");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.post("/test", (req: Request, res: Response) => {
+  return res.status(200).json({ msg: "Test is working" });
+});
+
+const port = 3500;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
